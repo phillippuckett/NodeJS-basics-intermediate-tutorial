f1f86a8609b8debe2f9ffc95ea6288f1fcc4eba9
Phillip Puckett <phillippuckett88@gmail.com>
2016-01-15 10:32:40 -0700

    Personal API 2/9

--- personal-api/index.js
+++ personal-api/index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var bodyParser = require('body-parser');
+var middleware = require('./controllers/middleware.js');
+var mainCtrl = require('./controllers/mainCtrl.js');
 
 var port = 5555;
 
 var app = express();
 
 app.use(bodyParser.json());
+app.use(middleware.addHeader);
 
 app.listen(port, function (){
     console.log("Listening on port, " + port)
---
+++ personal-api/interface/js/mainCtrl.js
@@ -0,0 +1,8 @@
+var exports = module.exports = {}
+
+exports.myFunction = function(req, res) {
+};
+
+exports.anotherFunction = function(req, res) {
+};
+
