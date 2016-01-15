2cd9cae77ee559858b1dd20d4da725795cb52746
Phillip Puckett <phillippuckett88@gmail.com>
2016-01-15 14:05:16 -0700

    Personal API 5/9

--- personal-api/controllers/mainCtrl.js
+++ personal-api/controllers/mainCtrl.js
@@ -2,20 +2,6 @@ var personalApi = require('');
 
 module.exports = {};
 
-/* GET NAME //
-exports.getName = function (req, res, next) {
-    res.status(200).json({
-        name: personalApi.name
-    })
-}; */
-
-/* GET LOCATION //
-exports.getLocation = function (req, res, next) {
-    res.status(200).json({
-        location: personalApi.location
-    })
-}; */
-
 exports.getOccupations = function (req, res, next) {
     var occupations = personalApi.occupations;
     if (req.query.order === 'desc') {
@@ -25,53 +11,3 @@ exports.getOccupations = function (req, res, next) {
     }
     res.status(200).json({ occupations: occupations });
 };
-
-/* GET LATEST OCCUPATIONS //
-exports.getLatestOccupations = function (req, res, next) {
-    var occupations = personalApi.occupations;
-    var latestOccupations = occupations[occupations.length - 1];
-    res.status(200).json({ latestOccupation: latestOccupations });
-}; */
-
-/* GET HOBBIES //
-exports.getHobbies = function (req, res, next) {
-    var hobbies = personalApi.hobbies;
-    if (req.query.order === 'desc') {
-        hobbies = hobbies.sort(function (x, y) {
-            if (x.name < y.name) {
-                return 1;
-            } else if (x.name = y.name) {
-                return 0;
-            } else {
-                return -1;
-            }
-        });
-
-    } else if (req.query.order === 'asc') {
-        hobbies = hobbies.sort(function (x, y) {
-            if (x.name < y.name) {
-                return 1;
-            } else if (x.name = y.name) {
-                return 0;
-            } else {
-                return -1;
-            }
-        });
-    }
-    res.status(200).json({ hobbies: hobbies });
-}; */
-
-/* GET HOBBIES BY TYPE //
-exports.getHobbiesByType = function (res, req, next) {
-    var hobbies = personalApi.hobbies;
-    var type = req.params.type;
-    var hobbiesByType = [];
-
-    for (var i = 0; i < hobbies.length; i++) {
-        var currentHobby = hobbies[i];
-        if (currentHobby.type === type) {
-            hobbiesByType.push(currentHobby);
-        }
-    }
-    res.status(200).json({ hobbiesByType: hobbiesByType });
-}; */
---
+++ personal-api/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var middleware = require('./controllers/middleware.js');
+var mainCtrl = require('./controllers/mainCtrl.js');
+
+var port = 5555;
+
+var app = express();
+
+app.use(bodyParser.JSON());
+app.use(middleware.addHeaders);
+
+// GET //
+app.get('/name', mainCtrl.getName);
+app.get('/location', mainCtrl.getLocation);
+app.get('/occupations', mainCtrl.getOccupations);
+app.get('/occupations/latest', mainCtrl.getLatestOccupation);
+app.get('/hobbies', mainCtrl.getHobbies);
+app.get('/hobbies/:type', middleware.getHobbiesByType);
+
+// UPDATE with PUT//
+app.put('/name', middleware.updateName);
+app.put('/location', mainCtrl.updateLocation);
+
+// UPDATE with POST //
+app.post('/hobbies', mainCtrl.updateHobbies);
+app.postt('/occupations', mainCtrl.updateOccupations);
+
+app.listen(port, function () {
+    console.log("Listening on port, " + port)
+});
